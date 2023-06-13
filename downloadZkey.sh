aec7fdd1dfe97e115687f1b7eb25c4c9adcccc3f
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2023-06-13 20:42:56 +0100

    store zkey files on external storage

---
+++ downloadZkey.sh
@@ -0,0 +1,22 @@
+# download a file and save it in ./build/circuits
+echo "Downloading zkey files..."
+echo "Downloading circuit_final.zkey.gz-part1"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part1 https://histopia.io/circuit_final.zkey.gz.sf-part1
+
+echo "Downloading circuit_final.zkey.gz-part2"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part2 https://histopia.io/circuit_final.zkey.gz.sf-part2
+
+echo "Downloading circuit_final.zkey.gz-part3"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part3 https://histopia.io/circuit_final.zkey.gz.sf-part3
+
+echo "Downloading circuit_final.zkey.gz-part4"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part4 https://histopia.io/circuit_final.zkey.gz.sf-part4
+
+echo "Downloading circuit_final.zkey.gz-part5"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part5 https://histopia.io/circuit_final.zkey.gz.sf-part5
+
+echo "Downloading circuit_final.zkey.gz-part6"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part6 https://histopia.io/circuit_final.zkey.gz.sf-part6
+
+echo "Downloading circuit_final.zkey.gz-part7"
+wget -O ./build/circuits/circuit_final.zkey.gz.sf-part7 https://histopia.io/circuit_final.zkey.gz.sf-part7
