12aecd381e8c1d07983342f31aa0218137d09104
Saber Coder <45599139+SaberDoTcodeR@users.noreply.github.com>
2024-02-03 14:10:17 +0000

    add miner & APSwap

---
+++ tests/Miner.spec.ts
@@ -0,0 +1,109 @@
+import {Blockchain, SandboxContract, TreasuryContract} from '@ton-community/sandbox';
+import {Address, beginCell, Cell, toNano} from 'ton-core';
+import '@ton-community/test-utils';
+import {compile} from '@ton-community/blueprint';
+import {JettonMinter} from "../wrappers/JettonMinter";
+import {JettonWallet} from "../wrappers/JettonWallet";
+import {APSwap} from "../wrappers/APSwap";
+
+
+
+describe('APSwap', () => {
+  let code: Cell;
+  let codeWallet: Cell;
+  let codeMaster: Cell;
+  async function getJettonBalance(address: Address) {
+    const jettonWallet = await blockchain.openContract(
+        JettonWallet.createFromAddress(await jettonMaster.getWalletAddress(address)))
+    return (await jettonWallet.getBalance());
+  }
+  beforeAll(async () => {
+    code = await compile('Miner');
+    codeWallet = await compile('JettonWallet');
+    codeMaster = await compile('JettonMinter');
+
+  });
+
+  let blockchain: Blockchain;
+  let apswap: SandboxContract<APSwap>;
+  let owner: SandboxContract<TreasuryContract>;
+  let jettonMaster: SandboxContract<JettonMinter>;
+  let start_time = 0;
+
+  beforeEach(async () => {
+    blockchain = await Blockchain.create();
+    owner = await blockchain.treasury('owner');
+
+    const deployer = await blockchain.treasury('deployer');
+    jettonMaster = blockchain.openContract(JettonMinter.createFromConfig({
+      adminAddress: owner.address,
+      content: "",
+      jettonWalletCode: codeWallet
+    }, codeMaster));
+    const deployJettonResult = await jettonMaster.sendDeploy(owner.getSender(), toNano('0.05'));
+    expect(deployJettonResult.transactions).toHaveTransaction({
+      from: owner.address,
+      to: jettonMaster.address,
+      deploy: true,
+      success: true,
+    });
+
+
+    apswap = blockchain.openContract(
+      APSwap.createFromConfig(
+        {
+          JettonMasterAddress: jettonMaster.address,
+            ADMIN_ADDRESS: owner.address,
+            MINER_ADDRESS: owner.address,
+            start: deployJettonResult.transactions[0].now
+        },
+        code
+      )
+    );
+    start_time = deployJettonResult.transactions[0].now;
+
+
+    const deployResult = await apswap.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: apswap.address,
+      deploy: true,
+      success: true,
+    });
+    await jettonMaster.sendMintAccess(owner.getSender(),{
+      value: toNano('0.02'),
+      queryId: 0,
+      mintAccess: apswap.address
+    })
+
+  });
+  it('should deploy and check', async () => {
+    let ONE_DAY = 86400;
+    blockchain.now = start_time + ONE_DAY * 364;
+    // console.log(await apswap.getTONNELVirtualBalance());
+    console.log(await apswap.getExpectedReturn(BigInt(100 * 86400)));
+    console.log(await apswap.getExpectedReturn(BigInt(10 * 86400)));
+  for (let i = 0; i < 100; i++) {
+    const resSwap = await apswap.sendSwap(owner.getSender(),
+        toNano('0.1'),
+        BigInt(100 * 86400),
+        owner.address
+    );
+    expect(resSwap.transactions).toHaveTransaction({
+      from: owner.address,
+      to: apswap.address,
+      success: true,
+    });
+    console.log(await apswap.getTokenSold());
+  }
+
+
+    console.log(await apswap.getExpectedReturn(BigInt(100 * 86400)));
+    console.log(await apswap.getExpectedReturn(BigInt(10 * 86400)));
+
+  });
+
+
+
+});
---
+++ wrappers/APSwap.ts
@@ -0,0 +1,102 @@
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider, Dictionary, DictionaryValue,
+    Sender,
+    SendMode, toNano
+} from 'ton-core';
+
+export type APSwapConfig = {
+    JettonMasterAddress: Address;
+    ADMIN_ADDRESS: Address;
+    MINER_ADDRESS: Address;
+    start: number;
+};
+
+const CellRef: DictionaryValue<Cell> = {
+    serialize: (src, builder) => {
+        builder.storeSlice(src.beginParse())
+    },
+    parse: (src) => src.asCell(),
+}
+
+export function tonnelConfigToCell(config: APSwapConfig): Cell {
+
+    return beginCell().storeAddress(config.JettonMasterAddress)
+        .storeAddress(config.ADMIN_ADDRESS)
+        .storeAddress(config.MINER_ADDRESS)
+        .storeUint(config.start, 32)
+        .storeCoins(0)
+        .endCell();
+
+}
+
+export const Opcodes = {
+    swap: 0x777,
+};
+export const ERRORS = {
+
+};
+
+export class APSwap implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+    }
+
+    static createFromAddress(address: Address) {
+        return new APSwap(address);
+    }
+
+    static createFromConfig(config: APSwapConfig, code: Cell, workchain = 0) {
+        const data = tonnelConfigToCell(config);
+        const init = {code, data};
+        return new APSwap(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendSwap(provider: ContractProvider, via: Sender, value: bigint, howMuch: bigint, to: Address) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(Opcodes.swap, 32)
+                .storeUint(0, 64)
+                .storeCoins(howMuch).
+                storeAddress(to).endCell(),
+        });
+    }
+
+    async getBalance(provider: ContractProvider) {
+        const result = await provider.getState();
+        return result.balance;
+    }
+
+    async getExpectedReturn(provider: ContractProvider, amount: bigint) {
+        const result = await provider.get('get_expected_return', [
+            {type: 'int', value: amount},
+        ]);
+        return result.stack.readBigNumber();
+    }
+
+
+    async getTONNELVirtualBalance(provider: ContractProvider) {
+        const result = await provider.get('get_tonnel_virtual_balance', []);
+        return result.stack.readBigNumber();
+    }
+
+
+    async getTokenSold(provider: ContractProvider) {
+        const result = await provider.get('get_token_sold', []);
+        return result.stack.readBigNumber();
+    }
+
+
+}
